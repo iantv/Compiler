a633ae4e2a2ad9dd0746a5b2b43c745eecf85b03
iantv <ian.tatiana.v@gmail.com>
2015-10-28 21:53:01 +1000

    Add basic class expr and its childs(with some methods)

--- parser.cpp
+++ parser.cpp
@@ -1,3 +1,25 @@
 #include "parser.h"
 
+/* methods of class expr */
+expr_bin_op::expr_bin_op(expr *left, expr *right, token t):l(left), r(right){ tk = t; }
+expr_const::expr_const(token t){ tk = t; }
+expr_var::expr_var(token t){ tk = t; }
+
+void expr_bin_op::print(ostream &os, int level){
+}
+
+void expr_const::print(ostream &os, int level){
+}
+
+void expr_var::print(ostream &os, int level){
+}
+
+/* methods of class parser */
 parser::parser(lexer *l): lxr(l) {}
+
+void parser::parse(){
+}
+
+void parser::print(ostream &os){
+	e->print(os, 1);
+}
--- parser.h
+++ parser.h
@@ -3,10 +3,42 @@
 
 #include "lexer.h"
 
+class expr{
+protected:
+	token tk;
+public:
+	virtual void print(ostream &os, int level) = 0;
+};
+
+class expr_bin_op: public expr{
+	expr *l, *r;
+public:
+	expr_bin_op(expr *left, expr *right, token t);
+	void print(ostream &os, int level);
+};
+
+class expr_const:expr{
+public:
+	expr_const(token t);
+	void print(ostream &os, int level);
+};
+
+class expr_var:expr{
+public:
+	expr_var(token t);
+	void print(ostream &os, int level);
+};
+
 class parser{
+	expr *e;
 	lexer *lxr;
+	expr *expression();
+	expr *term();
+	expr *factor();
 public:
 	parser(lexer *l);
+	void parse();	
+	void print(ostream &os);
 };
 
 #endif
