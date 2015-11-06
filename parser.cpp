e686a6f6e86965fb45ce8ad639bab2b6235a2678
iantv <ian.tatiana.v@gmail.com>
2015-11-06 22:07:14 +1000

    Add priority of operators

--- parser.cpp
+++ parser.cpp
@@ -1,52 +1,14 @@
 #include "parser.h"
 
-expr_bin_op::expr_bin_op(expr *left, expr *right, token t): l(left), r(right){ tk = t; }
-expr_literal::expr_literal(token t){ tk = t; }
-expr_var::expr_var(token t){ tk = t; }
-
-void expr::print_level(ostream &os, int level){
-	while (level){
-		os << '\t';
-		level--;
-	}
-}
-
-void expr_bin_op::print(ostream &os, int level){
-	r->print(os, level + 1);
-	print_level(os, level);
-	os << tk.get_src() << endl;
-	l->print(os, level + 1);
-}
-
-void expr_literal::print(ostream &os, int level){
-	print_level(os, level);
-	os << tk.get_src() << endl;
-}
-
-void expr_var::print(ostream &os, int level){
-	print_level(os, level);
-	os << tk.get_src() << endl;
-}
-
 parser::parser(lexer *l): lxr(l) {}
 
-expr *parser::expression(){
-	expr *ex = term();
+expr *parser::expression(int priority){
+	if (priority > MAX_PRIORITY) return factor();		
+	expr *ex = expression(priority + 1);
 	token tk = lxr->get();
-	while (tk.type == TK_PLUS || tk.type == TK_MINUS){
+	while (get_priority(tk) == priority){	/* DO not only binop */ 
 		lxr->next();
-		ex = new expr_bin_op(ex, term(), tk);
-		tk = lxr->get();
-	}
-	return ex;
-}
-
-expr *parser::term(){
-	expr *ex = factor();
-	token tk = lxr->get();
-	while (tk.type == TK_MUL || tk.type == TK_DIV){
-		lxr->next();
-		ex = new expr_bin_op(ex, factor(), tk);
+		ex = new expr_bin_op(ex, expression(priority + 1), tk);
 		tk = lxr->get();
 	}
 	return ex;
@@ -59,7 +21,7 @@ expr *parser::factor(){
 		return new expr_var(tk);
 	}
 	if (tk.type == TK_OPEN_BRACKET){
-		expr *ex = expression();
+		expr *ex = expression(1);
 		if (tk.type == TK_CLOSE_BRACKET)
 			throw 1;
 		lxr->next();
@@ -68,14 +30,9 @@ expr *parser::factor(){
 	if (tk.type == TK_INT_VAL || tk.type == TK_DOUBLE_VAL || tk.type == TK_CHAR_VAL){
 		return new expr_literal(tk);
 	}
-	return e;
+	//return e;
 }
 
-void parser::parse(){
-	lxr->next();
-	e = expression();	
-}
-
-void parser::print(ostream &os){
-	e->print(os, 0);
+expr *parser::parse_expr(){
+	return expression(MIN_PRIORITY);
 }
--- parser.h
+++ parser.h
@@ -2,45 +2,15 @@
 #define PARSER
 
 #include "lexer.h"
-#include <stack>
-
-class expr{
-protected:
-	token tk;
-public:
-	virtual void print(ostream &os, int level) = 0;
-	void print_level(ostream &os, int level);
-};
-
-class expr_bin_op: public expr{
-	expr *l, *r;
-public:
-	expr_bin_op(expr *left, expr *right, token t);
-	void print(ostream &os, int level);
-};
-
-class expr_literal: public expr{
-public:
-	expr_literal(token t);
-	void print(ostream &os, int level);
-};
-
-class expr_var: public expr{
-public:
-	expr_var(token t);
-	void print(ostream &os, int level);
-};
+#include "expression.h"
 
 class parser{
-	expr *e;
 	lexer *lxr;
-	expr *expression();
-	expr *term();
+	expr *expression(int priority);
 	expr *factor();
 public:
 	parser(lexer *l);
-	void parse();	
-	void print(ostream &os);
+	expr *parse_expr();
 };
 
 #endif
