23b4729453f62047a9c58547a89c9bdd7fd9d34e
iantv <ian.tatiana.v@gmail.com>
2015-11-06 22:08:05 +1000

    init tests

---
+++ Tests.cpp
@@ -0,0 +1,16 @@
+#include "lexer.h"
+#include "parser.h"
+#include <string>
+
+#include <fstream>
+
+#define ltests "Tests\lexer"
+#define lcnt 4
+#define ptests "Tests\parser"
+
+void lexer_tests(){
+	for (int i = 0; i < lcnt; i++){
+		FILE *fin, *fout;
+		
+	}
+}
