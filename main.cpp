9aae733c822939d725ace7aaf311a9334e2c9142
iantv <ian.tatiana.v@gmail.com>
2015-11-13 16:31:03 +1000

    ...

--- main.cpp
+++ main.cpp
@@ -3,13 +3,17 @@
 #include "lexer.h"
 #include "parser.h"
 #include "Tests.h"
+//#include <math.h>
 
+int a, b, c, d, e, f, g, h, i, j, k;
 int main(int argc, const char *argv[]){
 	if (argc == 1) {
 		cout<<"fatal error: no input files\ncompilation terminated\n";
 	}
+	b = c = d = e = f = g = h = i = j = k = 1;
+	a=((((((((((b+=1)-=c)*=d)/=e)%=f)^=g)|=h)&=i)<<=j)>>=k);
 	
-	tests T(UT_LEXER);
+	tests T = tests();
 	T.lexer_tests();
 	cout << endl;
 	T.parser_tests();
