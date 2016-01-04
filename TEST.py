70cbf1d26b169c8997ec95be59bac9b7d813ef72
Tatiana <ian.tatiana.v@gmail.com>
2016-01-04 16:22:20 +1000

    Add tests for functions rerurns structures

--- TEST.py
+++ TEST.py
@@ -2,9 +2,9 @@ import subprocess
 LTESTCNT = 40
 PTESTCNT = 50
 
-PDTESTCNT = 24
+PDTESTCNT = 25
 
-ERRPRSDCL = 9 #Error Parser Declare
+ERRPRSDCL = 10 #Error Parser Declare
 def testproc(testname, cmd, N, output, ansdir):
 	print testname
 	for i in range(1, N + 1):
