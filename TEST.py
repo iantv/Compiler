507b63c98ebd2e989a748173f27ec2a00afaa8c9
Tatiana <ian.tatiana.v@gmail.com>
2016-02-04 20:33:52 +1000

    Change count of tests for expressions

--- TEST.py
+++ TEST.py
@@ -1,6 +1,6 @@
 import subprocess
 LTESTCNT = 40
-PTESTCNT = 50
+EXPR = 51
 
 PDTESTCNT = 49
 
@@ -19,7 +19,7 @@ def testproc(testname, cmd, N, output, ansdir):
 	print
 
 testproc('Lexical analysis','-l', LTESTCNT, 'lexer.out', 'Tests/lexer/')
-testproc('Expression parsing', '-p -expr', PTESTCNT, 'expression.out', 'Tests/expr/')
+testproc('Expression parsing', '-p -expr', EXPR, 'expression.out', 'Tests/expr/')
 testproc('Declarations parsing', '-p -decl', PDTESTCNT, 'declar.out', 'Tests/declar/')
 testproc('', '-p -decl', ERRPRSDCL, 'declar.out', 'Tests/errors/declar/')
 #testproc('Type casting', '-p -tcast', TCAST, 'typecast.out', 'Tests/typecast/')
